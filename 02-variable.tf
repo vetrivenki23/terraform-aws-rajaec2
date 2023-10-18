35b003b1b7b3856d283fc42994b9b71fba6e9fe4
vetrivenki23 <echlearnings23@gmail.com>
2023-10-17 21:56:06 -0500

    v1.0.1

--- 02-variable.tf
+++ 02-variable.tf
@@ -10,3 +10,10 @@ variable "ec2_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "ec2_tag" {
+  description = "A string variable"
+  type        = string
+  default     = "EC2-Raja"
+}
+
--- 05-output.tf
+++ 05-output.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = aws_instance.tf_ec2.public_ip
 }
+
+output "private_ip" {
+  value = aws_instance.tf_ec2.private_ip
+}
